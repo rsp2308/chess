b463b96f72e242e918542fb4eaf53f0498a08fb7
rsp2308 <roshan.parmar2024@lpu.in>
2025-01-04 19:07:56 +0530

    yaml

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
