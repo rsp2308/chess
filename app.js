aa6d2110aa6a50c049e51ced8da79087aba3a81f
rsp2308 <roshan.parmar2024@lpu.in>
2025-01-05 20:21:56 +0530

    drag

--- app.js
+++ app.js
@@ -22,12 +22,52 @@ app.get("/",(req,res)=>{
 
 io.on("connection",(uniquesocket)=>{
  console.log("connected")
+// role assigning
+ if (!players.white){
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole","W");
+ }
+ else if(!players.black){
+    players.black =uniquesocket.id;
+    uniquesocket.emit("playerRole","b")
+ }
+ else{
+    uniquesocket.emit("spectatorRole")
+ }
+ uniquesocket.on("disconnect",function(){
+    if(uniquesocket.id === players.white){
+        delete players.white;
+    }
+     else if(uniquesocket.id === players.black){
+        delete players.black
+    }
+ });
 
- uniquesocket.on("khana",function(){
-    console.log("churana khaa liya")
- })
-})
+uniquesocket.on("move",(move)=>{
+    try{
+        // move error 
+        if(chess.turn() === 'W' && uniquesocket.id !== players.white) return;
+        if(chess.turn() === 'b' && uniquesocket.id !== players.black) return;
 
-server.listen(3001,function(){
-    console.log("listening on port 3001");
+        const result = chess.move(move);
+        if(result){
+            currentPlayer - chess.turn();
+            io.emit("move",move);
+            io.emit("boardState",chess.fen())
+         }
+         else{
+         console.log("Invalid move:",move);
+         uniquesocket.emit("invalidMove",move);
+         }
+    }
+    catch(err){
+        console.log(err);
+        uniquesocket.emit("Invalid move: ", move);
+    }
+});
+
+});
+
+server.listen(3000,function(){
+    console.log("listening on port 3010");
 })
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,111 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
 
-socket.emit("khana")
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+// to create light dark pattern on board
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = squareindex;
+
+      // to set piece side
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+        pieceElement.innerText = getPieceUnicode(square);
+        pieceElement.draggable = playerRole === square.color;
+        pieceElement.addEventListener("dragstart", (e) => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowindex, col: squareindex };
+            e.dataTransfer.setData("text/plain", "");
+          }
+        });
+
+        pieceElement.addEventListener("dragged", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+
+        squareElement.appendChild(pieceElement);
+      }
+
+      squareElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+      squareElement.addEventListener("drop", function (e) {
+        e.preventDefault();
+        if (draggedPiece) {
+          const targetSource = {
+            row: parseInt(squareElement.dataset.row),
+            col: parseInt(squareElement.dataset.col),
+          };
+          handleMove(sourceSquare, targetSource);
+        }
+      });
+      boardElement.appendChild(squareElement);
+    });
+  });
+};
+
+const handleMove = (source,target) => {
+    const move = {
+        from: `${String.fromCharCode(97+source.col)}${8- source.row}`,
+        to:`${String.fromCharCode(97+target.col)}${8- target.row}`,
+        promotion:'q'
+    }
+    socket.emit("move",move);
+};
+
+const getPieceUnicode = (piece) => {
+  const unicodePieces = {
+    p: "♙",
+    r: "♜",
+    n: "♞",
+    b: "♝",
+    q: "♛ ",
+    k: "♚",
+    p: "♙",
+    R: "♖",
+    N: "♘",
+    B: "♗",
+    Q: "♕",
+    K: "♔",
+  };
+  return unicodePieces[piece.type] || "";
+};
+socket.on("playerRole",(role)=>{
+ playerRole = role;
+ renderBoard();
+});
+
+socket.on("spectatorRole",(role)=>{
+    playerRole=null;
+    renderBoard();
+});
+
+socket.on("boardState",(fen)=>{
+    chess.load(fen);
+    renderBoard();
+});
+socket.on("move",(fen)=>{
+    chess.move(fen);
+    renderBoard();
+});
+renderBoard();
